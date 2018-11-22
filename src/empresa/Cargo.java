f0001f46bbe877eb8f05ecda65b8370d97ee7936
Gabriel <gabrielthe13@gmail.com>
2018-11-21 23:32:34 -0200

    Testing my project  Changes to be committed: 	new file:   build.xml 	new file:   build/classes/.netbeans_automatic_build 	new file:   build/classes/.netbeans_update_resources 	new file:   build/classes/empresa/Cargo.class new file:   build/classes/empresa/Empresa.class 	new file:   build/classes/empresa/Funcionario.class 	new file:   build/classes/empresa/exception.class 	new file:   manifest.mf 	new file:   nbproject/build-impl.xml 	new file:   nbproject/genfiles.properties 	new file:   nbproject/private/private.properties new file:   nbproject/private/private.xml 	new file:   nbproject/project.properties 	new file:   nbproject/project.xml 	new file:   src/empresa/Cargo.java 	new file:   src/empresa/Funcionario.java 	new file:   src/empresa/exception.java

---
+++ src/empresa/Cargo.java
@@ -0,0 +1,57 @@
+
+package empresa;
+
+
+public class Cargo {
+    int cargo1;
+    int cargo2;
+    int cargo3;
+    int numcargo;
+    double valht;
+
+    public int getNumcargo() {
+        return numcargo;
+    }
+
+    public void setNumcargo(int carg) {
+    
+            this.numcargo = carg;
+        }
+
+    public double getValht() {
+        return valht;
+    }
+
+    public void setValht(double valht) {
+        this.valht = valht;
+    }
+
+    public int getCargo1() {
+        return cargo1;
+    }
+
+    public void setCargo1(int cargo1) {
+        this.cargo1 = cargo1;
+        
+    }
+
+    public int getCargo2() {
+        return cargo2;
+    }
+
+    public void setCargo2(int cargo2) {
+        this.cargo2 = cargo2;
+    }
+
+    public int getCargo3() {
+        return cargo3;
+    }
+
+    public void setCargo3(int cargo3) {
+        this.cargo3 = cargo3;
+    }
+  
+   
+    
+    
+}
---
+++ src/empresa/Funcionario.java
@@ -0,0 +1,70 @@
+
+package empresa;
+    
+public class Funcionario {
+    int códcargo;
+    String sexo;
+    double qhtb;
+    double sf;
+    Cargo Cargo;
+    double sln;
+    int value;
+    public double getSln() {
+        return sln;
+    }
+
+   public void setSln (double valht, double qhtb) {
+        this.sln = valht*qhtb;
+    }
+
+    public int getCódcargo() {
+        return códcargo;
+    }
+
+    public void setCódcargo(int códcargo) {
+        this.códcargo = códcargo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getQhtb() {
+        return qhtb;
+    }
+
+    public void setQhtb(double qhtb) {
+        this.qhtb = qhtb;
+    }
+
+    public double getSf() {
+        return sf;
+    }
+
+    public void setSf(double sln, int value) {
+       try { 
+           switch(sexo){
+            case "f":
+                this.sf =+ sln+(sln*20)/100;
+                break;
+            case "m":
+                this.sf = sln;
+                break;
+            default:
+                System.out.println("ERRO: Insira um sexo válido");
+                break;
+        }
+       }catch(Exception e){
+            System.out.println("Erro: " + e.getMessage());
+        }
+            
+
+    }
+    
+    
+}
+
